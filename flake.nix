38c37357758a96802fa8964a6bde59a128b03af0
Chris Skalenda <chris@skalenda.org>
2024-08-17 12:32:16 -0600

    Move nix-darwin config to its own file

--- flake.nix
+++ flake.nix
@@ -17,34 +17,6 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, ... }:
   let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.home-manager ];
-
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 4;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-
-      security.pam.enableSudoTouchIdAuth = true;
-    };
     system = "aarch64-darwin";
     pkgs = import nixpkgs { inherit system; };
   in
@@ -52,14 +24,14 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .
     darwinConfigurations."quail" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ ./configuration.nix ];
     };
 
     # Expose the package set, including overlays, for convenience.
     darwinPackages = self.darwinConfigurations."quail".pkgs;
 
     homeConfigurations = {
-      personal= home-manager.lib.homeManagerConfiguration {
+      personal = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [ ./home.nix ];
       };
