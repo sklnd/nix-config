9873a1521a156d9831f939615da3633503eecdb1
Chris Skalenda <chris@skalenda.org>
2024-08-17 11:24:38 -0600

    Add vim and git configuration in home manager

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ pkgs.vim pkgs.home-manager ];
+        [ pkgs.home-manager ];
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
--- home.nix
+++ home.nix
@@ -2,11 +2,71 @@
 {
   home = {
     packages = with pkgs; [
-      neovim tmux
+      tmux
     ];
     username = "chris";
     homeDirectory = "/Users/chris";
 
     stateVersion = "23.11";
+  };
+  programs = {
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+      plugins = with pkgs.vimPlugins; [
+        nvim-lspconfig
+        nvim-treesitter.withAllGrammars
+      ];
+    };
+
+    git = {
+      enable = true;
+      userName = "Chris Skalenda";
+      userEmail = "chris@skalenda.org";
+      aliases = {
+        "co" = "checkout";
+      };
+      extraConfig = {
+        "user" = {
+          signingkey = "/Users/chris/.ssh/2021-ecdsa";
+        };
+        "core" = {
+          editor = "vim";
+          autocrlf = false;
+          excludesfile = "~/.git_global_ignore";
+        };
+        "push" = {
+          default = "current";
+        };
+        "color" = {
+          "status" = {
+            added = "green bold";
+            changed = "red bold strike";
+            untracked = "cyan";
+            branch = "yellow black bold ul";
+          };
+        };
+        "pull" = {
+          ff = "only";
+        };
+        "filter" = {
+          "lfs" = {
+            clean = "git-lfs clean -- %f";
+            smudge = "git-lfs smudge -- %f";
+            process = "git-lfs filter-process";
+            required = true;
+          };
+        };
+        "init" = {
+          defaultBranch = "main";
+        };
+        "gpg" = {
+          format = "ssh";
+        };
+      };
+    };
   };
 }
