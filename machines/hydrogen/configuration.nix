1f5faeb47978c1b3df3874a7fb63a36ddfccb10b
Chris Skalenda <chris@skalenda.org>
2025-01-16 19:53:18 -0700

    Add initial nixos config for Hydrogen

---
+++ machines/hydrogen/configuration.nix
@@ -0,0 +1,60 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "hydrogen";
+  networking.networkmanager.enable = true;
+  time.timeZone = "America/Denver";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  users.users.chris = {
+    isNormalUser = true;
+    description = "Chris Skalenda";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+        git
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    vim
+  ];
+
+  services.openssh.enable = true;
+
+  services.plex = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
