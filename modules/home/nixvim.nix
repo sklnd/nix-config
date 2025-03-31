f58abc497450624146a03f656d59af76ff44aacf
Chris Skalenda <chris@skalenda.org>
2025-03-30 20:12:38 -0600

    [home] set up telescope

--- modules/home/nixvim.nix
+++ modules/home/nixvim.nix
@@ -45,6 +45,28 @@
           preserveWindowProportions = true;
         };
       };
+      plugins.telescope = {
+        enable = true;
+        keymaps = {
+          "<C-p>" = {
+            action = "git_files";
+            options = {
+              desc = "Telescope Git Files";
+            };
+          };
+          "<leader>fg" = "live_grep";
+        };
+        settings = {
+          defaults = {
+            layoutConfig = {
+              horizontal = {
+                previewWidth = 0.6;
+              };
+            };
+          };
+
+        };
+      };
 
       keymaps = [
         # NvimTree
