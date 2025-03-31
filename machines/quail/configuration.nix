a0421f28a00c836a2aaf91d878f26ab0689a522a
Chris Skalenda <chris@skalenda.org>
2025-03-30 19:04:37 -0600

    [quail] Update config for unstable release

--- machines/quail/configuration.nix
+++ machines/quail/configuration.nix
@@ -8,8 +8,7 @@ in
   nix.settings.experimental-features = "nix-command flakes";
   nixpkgs.hostPlatform = machineDefs.hostPlatform;
   programs.zsh.enable = true;
-  security.pam.enableSudoTouchIdAuth = true;
-  services.nix-daemon.enable = true;
+  security.pam.services.sudo_local.touchIdAuth = true;
   system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false;
 
   # Used for backwards compatibility, please read the changelog before changing.
