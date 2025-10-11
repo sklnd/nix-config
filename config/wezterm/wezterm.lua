35885ec562dd2b39599190f6d0a008a457d3fec2
Chris Skalenda <chris@skalenda.org>
2025-10-11 17:52:30 -0600

    [home] Change up wezterm tab config
    Use https://github.com/michaelbrusegard/tabline.wez for tabs

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 -- Pull in the wezterm API
 local wezterm = require('wezterm')
 local appearance = require('appearance')
+local tabline = wezterm.plugin.require('https://github.com/michaelbrusegard/tabline.wez')
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -12,70 +13,9 @@ else
 end
 
 config.window_decorations = 'RESIZE'
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.8
 config.macos_window_background_blur = 30
 config.window_close_confirmation = 'NeverPrompt'
-
-local function segments_for_right_status(window)
-    return {
-        window:active_workspace(),
-        wezterm.strftime('%a %b %-d %H:%M'),
-        wezterm.hostname(),
-    }
-end
-
-wezterm.on('update-status', function(window, _)
-    local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-    local segments = segments_for_right_status(window)
-
-    local color_scheme = window:effective_config().resolved_palette
-    -- Note the use of wezterm.color.parse here, this returns
-    -- a Color object, which comes with functionality for lightening
-    -- or darkening the colour (amongst other things).
-    local bg = wezterm.color.parse(color_scheme.background)
-    local fg = color_scheme.foreground
-
-    -- Each powerline segment is going to be coloured progressively
-    -- darker/lighter depending on whether we're on a dark/light colour
-    -- scheme. Let's establish the "from" and "to" bounds of our gradient.
-    local gradient_to, gradient_from = bg, nil
-    if appearance.is_dark() then
-        gradient_from = gradient_to:lighten(0.2)
-    else
-        gradient_from = gradient_to:darken(0.2)
-    end
-
-    -- Yes, WezTerm supports creating gradients, because why not?! Although
-    -- they'd usually be used for setting high fidelity gradients on your terminal's
-    -- background, we'll use them here to give us a sample of the powerline segment
-    -- colours we need.
-    local gradient = wezterm.color.gradient(
-        {
-            orientation = 'Horizontal',
-            colors = { gradient_from, gradient_to },
-        },
-        #segments -- only gives us as many colours as we have segments.
-    )
-
-    -- We'll build up the elements to send to wezterm.format in this table.
-    local elements = {}
-
-    for i, seg in ipairs(segments) do
-        local is_first = i == 1
-
-        if is_first then
-            table.insert(elements, { Background = { Color = 'none' } })
-        end
-        table.insert(elements, { Foreground = { Color = gradient[i] } })
-        table.insert(elements, { Text = SOLID_LEFT_ARROW })
-
-        table.insert(elements, { Foreground = { Color = fg } })
-        table.insert(elements, { Background = { Color = gradient[i] } })
-        table.insert(elements, { Text = ' ' .. seg .. ' ' })
-    end
-
-    window:set_right_status(wezterm.format(elements))
-end)
 
 -- Table mapping keypresses to actions
 config.keys = {
@@ -107,5 +47,53 @@ config.font = wezterm.font({
     harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
 })
 config.font_shaper = 'Harfbuzz'
+
+tabline.setup({
+    options = {
+        icons_enabled = true,
+        theme = 'Catppuccin Mocha',
+        tabs_enabled = true,
+        theme_overrides = {},
+        section_separators = {
+            left = wezterm.nerdfonts.pl_left_hard_divider,
+            right = wezterm.nerdfonts.pl_right_hard_divider,
+        },
+        component_separators = {
+            left = wezterm.nerdfonts.pl_left_soft_divider,
+            right = wezterm.nerdfonts.pl_right_soft_divider,
+        },
+        tab_separators = {
+            right = wezterm.nerdfonts.ple_left_half_circle_thick,
+            left = wezterm.nerdfonts.ple_right_half_circle_thick,
+        },
+    },
+    sections = {
+        tabline_a = { 'hostname' },
+        tabline_b = '',
+        tabline_c = '',
+        tab_active = {
+            'index',
+            { 'parent', padding = 0 },
+            '/',
+            { 'cwd', padding = { left = 0, right = 1 } },
+            { 'process', padding = { left = 0, right = 1 } },
+            { 'zoomed', padding = 0 },
+        },
+        tab_inactive = {
+            'index',
+            { 'parent', padding = 0 },
+            '/',
+            { 'cwd', padding = { left = 0, right = 1 } },
+            { 'process', padding = { left = 0, right = 1 } },
+            { 'zoomed', padding = 0 },
+        },
+        tabline_x = '',
+        tabline_y = '',
+        tabline_z = { 'ram', 'cpu' },
+    },
+    extensions = {},
+})
+
+tabline.apply_to_config(config)
 
 return config
