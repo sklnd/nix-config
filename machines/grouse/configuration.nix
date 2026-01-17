399c659908e79ae066798a6d5284693de051a780
Chris Skalenda <chris@skalenda.org>
2026-01-16 17:36:56 -0700

    Add dns config to docker

--- machines/grouse/configuration.nix
+++ machines/grouse/configuration.nix
@@ -137,6 +137,12 @@ in
 
   virtualisation.docker = {
     enable = true;
+    daemon.settings = {
+      dns = [
+        "1.1.1.1"
+        "8.8.8.8"
+      ];
+    };
   };
 
   environment.systemPackages = with pkgs; [
