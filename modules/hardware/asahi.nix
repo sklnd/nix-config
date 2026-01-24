491a9747cc7f0c8cf94f2c7a4d7840299395dfb3
Chris Skalenda <chris@skalenda.org>
2026-01-23 18:34:46 -0700

    [grouse] Set keyboard backlight on boot

--- modules/hardware/asahi.nix
+++ modules/hardware/asahi.nix
@@ -1,11 +1,17 @@
 # Configuration necessary for MBP hardware
 {pkgs, ...}: {
   # F keys active by default, fn to use alt function
-  boot.extraModprobeConfig = ''
-    options hid_apple fnmode=2
-  '';
+  boot = {
+    extraModprobeConfig = ''
+      options hid_apple fnmode=2
+    '';
 
+    kernel.sysctl = {
+      # Max brightness is 255.
+      "class.leds.kbd_backlight.brightness" = 100;
+    };
+  };
   environment.systemPackages = with pkgs; [
-    brightnessctl
+    brightnessctl # screen backlight
   ];
 }
