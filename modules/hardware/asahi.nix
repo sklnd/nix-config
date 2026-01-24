6fb016bc35c2473d13d4f7b1a3b59cdb36333482
Chris Skalenda <chris@skalenda.org>
2026-01-23 18:31:19 -0700

    [grouse] Use function keys by default

--- modules/hardware/asahi.nix
+++ modules/hardware/asahi.nix
@@ -1,6 +1,10 @@
 # Configuration necessary for MBP hardware
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
+  # F keys active by default, fn to use alt function
+  boot.extraModprobeConfig = ''
+    options hid_apple fnmode=2
+  '';
+
   environment.systemPackages = with pkgs; [
     brightnessctl
   ];
