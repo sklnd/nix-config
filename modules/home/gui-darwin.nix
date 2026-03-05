61c2af85b62288229612bef83749f4a61dbcec8d
Chris Skalenda <chris.skalenda@joinhonor.com>
2026-03-04 20:04:50 -0700

    darwin: install wezterm

--- modules/home/gui-darwin.nix
+++ modules/home/gui-darwin.nix
@@ -7,8 +7,11 @@
     enable = false;
   };
 
+
+
   config = lib.mkIf (pkgs.stdenv.isDarwin) {
     xdg.configFile."sketchybar".source = ../../config/sketchybar;
+    programs.wezterm.enable = true;
     home.sessionPath = [
       # Temp hack until vscode is managed by home-manager
       "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/"
