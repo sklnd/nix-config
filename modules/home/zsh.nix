eb1a1314058556ac0bab723cea4243456f84cfba
Chris Skalenda <chris@skalenda.org>
2026-04-05 17:17:33 -0600

    home: use bitwarden for ssh agent

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -15,13 +15,28 @@
     zsh = {
       enable = true;
       dotDir = "${config.xdg.configHome}/zsh";
-      initContent = lib.mkMerge [
-        (lib.mkBefore ''
-          # Powerlevel10k Zsh theme
-          source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-        '')
-        (lib.mkOrder 550 (builtins.readFile ../../config/zsh/zshrc))
-      ];
+      initContent = let
+        bitawardenSSHSockPath =
+          if pkgs.stdenv.isDarwin
+          then ''
+            ${config.home.homeDirectory}/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock
+          ''
+          else ''
+            ${config.home.homeDirectory}/.bitwarden-ssh-agent.sock
+          '';
+      in
+        lib.mkMerge [
+          (lib.mkBefore ''
+            # Powerlevel10k Zsh theme
+            source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+          '')
+          (lib.mkOrder 550 (builtins.readFile ../../config/zsh/zshrc))
+          (
+            lib.mkOrder 1000 ''
+              export SSH_AUTH_SOCK=${bitawardenSSHSockPath}
+            ''
+          )
+        ];
       oh-my-zsh = {
         enable = true;
         plugins =
